c5fa4ecf58fb5918041abde8cb65965a2b78c180
Ondrej Platek <ondrej.platek@seznam.cz>
2016-04-27 11:19:00 +0200

    merge and fix accuracy

--- fat_demo.py
+++ fat_demo.py
@@ -14,24 +14,35 @@ __author__ = 'Petr Belohlavek, Vojtech Hudecek'
 
 
 if __name__ == '__main__':
+    # Don't forget to install bleeding-edge TF version
+    # sudo pip3 install http://ci.tensorflow.org/view/Nightly/job/nightly-matrix-cpu/TF_BUILD_CONTAINER_TYPE\=CPU,TF_BUILD_IS_OPT\=OPT,TF_BUILD_IS_PIP\=PIP,TF_BUILD_PYTHON_VERSION\=PYTHON3,label\=cpu-slave/lastSuccessfulBuild/artifact/pip_test/whl/tensorflow-0.8.0rc0-cp34-cp34m-linux_x86_64.wh
+    # See Installation for other versions https://github.com/tensorflow/tensorflow/blob/master/README.md
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-    # Config
+    input_val = np.array([[1, 2, 2, 4],
+                          [1, 3, 3, 4],
+                          [1, 0, 0, 4]], dtype='int32')
+    # labels_val = np.array([[1, 0, 0],
+    #                        [0, 1, 0]], dtype='float32')
+    labels_val = np.array([0, 1, 2], dtype='int64')
+
+    assert input_val.shape[0] == labels_val.shape[0]
+
     c = Config()
+    c.log_dir = 'log/'
     c.seed = 123
-    c.epochs = 20
+    c.epochs = 200
     c.learning_rate = 0.005
     random.seed(c.seed)
 
-    c.vocab_size = 5
-    c.labels_size = 3
-    c.max_seq_len = 4
-    c.batch_size = 1
+    c.vocab_size = np.max(input_val) + 1
+    c.batch_size = labels_val.shape[0]
+    c.output_dim = np.max(labels_val) + 1
+    c.max_seq_len = input_val.shape[1]
     c.embedding_dim = 10
     c.hidden_state_dim = 50
-    c.mlp_hidden_layer_dim = 10
 
     # Fun part
     logger.info('Creating model')
@@ -48,24 +59,23 @@ if __name__ == '__main__':
     logger.info('Run init')
     sess.run(init)
 
-    input_val = np.array([[1, 2, 3, 4],
-                          [4, 3, 2, 1]], dtype='int32')
-    labels_val = np.array([[1, 0, 0],
-                           [0, 1, 0]], dtype='float32')
+    logger.info('See stats via tensorboard\n\ntensorboard --logdir %s\n\n', c.log_dir)
+    train_writer = tf.train.SummaryWriter(c.log_dir, sess.graph)
 
     # Training part
+    epoch_loss = 0
+    train_summary = None
     for e in range(c.epochs):
         skipped_dial = 0
-        for step in range(10000):
-            for i in range(input_val.shape[0]):
-                _, loss = sess.run([train_op, m.loss], feed_dict={m.input: [input_val[i, :]], m.labels: [labels_val[i, :]]})
+        for step in range(100):
+            _, epoch_loss, train_summary= sess.run([train_op, m.loss, m.tb_info], feed_dict={m.input: input_val, m.labels: labels_val})
+            # epoch_loss = loss
+        train_writer.add_summary(train_summary, e)
 
-            if step % 100 == 0:
-                logger.debug('Average Batch Loss @%d/%d = %f', e, step, loss)
-                for i in range(input_val.shape[0]):
-                    results = sess.run([m.predict, m.logits, m.emb], feed_dict={m.input: [input_val[i, :]]})
-                    print(results[2])
-                    logging.debug('inputs: \n%s', input_val[i, :])
-                    logging.debug('labels: \n%s', labels_val[i, :])
-                    logging.debug('Probs:\n%s', results[0])
-                    logging.debug('logits:\n%s', results[1])
+        logger.debug('Average Batch Loss @%d = %f', e, epoch_loss)
+        results = sess.run([m.probabilities, m.logits, m.predict, m.accuracy],
+                           feed_dict={m.input: input_val, m.labels: labels_val})
+        logging.debug('Probs:\n%s', results[0])
+        logging.debug('Logits:\n%s', results[1])
+        logging.debug('Predict:\n%s', results[2])
+        logging.debug('Accuracy:\n%s', results[3])
--- tracker/fat_model.py
+++ tracker/fat_model.py
@@ -3,7 +3,6 @@
 
 import tensorflow as tf
 from tensorflow.python.ops.rnn_cell import GRUCell
-
 __author__ = 'Petr Belohlavek, Vojtech Hudecek'
 
 
@@ -17,19 +16,19 @@ class FatModel:
     m = MLP hidden layer dimension
     """
     def __init__(self, config):
-        self.gru_n_steps = config.max_seq_len
-        self.state_dim = config.hidden_state_dim
-        self.output_dim = config.labels_size
-        self.vocab_size = config.vocab_size
-        self.emb_dim = config.embedding_dim
-        self.batch_size = config.batch_size
+        self.config = config
 
-        self.input = tf.placeholder("int32", [self.batch_size, config.max_seq_len], name='input')
-        self.labels = tf.placeholder("float32", [self.batch_size, self.output_dim], name='labels')  # FIXME convert index to one-hot vector in TF
+        self.input = tf.placeholder('int32', [self.config.batch_size, config.max_seq_len], name='input')
+        self.labels = tf.placeholder('int64', [self.config.batch_size], name='labels')
+        self.labels_one_hot = tf.one_hot(indices=self.labels,
+                                         depth=config.output_dim,
+                                         on_value=1.0,
+                                         off_value=0.0,
+                                         axis=-1)
 
-        self.gru = GRUCell(self.state_dim)
+        self.gru = GRUCell(config.hidden_state_dim)
 
-        embeddings_we = tf.get_variable('word_embeddings', initializer=tf.random_uniform([self.vocab_size, self.emb_dim], -1.0, 1.0))
+        embeddings_we = tf.get_variable('word_embeddings', initializer=tf.random_uniform([config.vocab_size, config.embedding_dim], -1.0, 1.0))
         self.emb = embed_input = tf.nn.embedding_lookup(embeddings_we, self.input)
         inputs = [tf.squeeze(i, squeeze_dims=[1]) for i in tf.split(1, config.max_seq_len, embed_input)]
 
@@ -38,7 +37,15 @@ class FatModel:
             inputs=inputs,
             dtype=tf.float32,)
 
-        w_project = tf.get_variable('project2labels', initializer=tf.random_uniform([self.state_dim, self.output_dim], -1.0, 1.0))
+        w_project = tf.get_variable('project2labels', initializer=tf.random_uniform([config.hidden_state_dim, config.output_dim], -1.0, 1.0))
         self.logits = logits_bo = tf.matmul(last_slu_state, w_project)
-        self.loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits_bo, tf.argmax(self.labels, 1)))
+        tf.histogram_summary('logits', logits_bo)
+        self.probabilities = tf.nn.softmax(logits_bo)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits_bo, self.labels_one_hot))
         self.predict = tf.nn.softmax(logits_bo)
+
+        # TensorBoard
+        self.accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.predict, 1), self.labels), 'float32'), name='accuracy')
+        tf.scalar_summary('CCE loss', self.loss)
+        tf.scalar_summary('Accuracy', self.accuracy)
+        self.tb_info = tf.merge_all_summaries()
